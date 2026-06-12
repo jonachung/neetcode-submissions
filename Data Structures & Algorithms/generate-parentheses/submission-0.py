02aa3da49fd270dde36008e2d5db4c403ff73b9a
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2026-06-12 10:02:13 -0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # answer = []
+        # backtracking with (curr, left, right) left parenthesis, right parenthesis
+        # if left == 0 and right == 0:
+        # answer.append(curr[:]), return
+
+        # if left < n
+        # curr.append("(")
+        # backtrack(curr, left - 1, right)
+        # curr.pop
+
+        # if right < left
+        # curr.append(")")
+        # backtrack(curr, left, right - 1)
+        # curr.pop
+
+        # backtrack(n,n)
+
+        answer = []
+
+        def backtrack(curr, left, right):
+            if left == n and right == n:
+                answer.append("".join(curr[:]))
+                return
+
+            if left < n:
+                curr.append("(")
+                backtrack(curr, left + 1, right)
+                curr.pop()
+
+            if right < left:
+                curr.append(")")
+                backtrack(curr, left, right + 1)
+                curr.pop()
+
+        backtrack([], 0, 0)
+        return answer
