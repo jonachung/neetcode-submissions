4a806a43ac396e001a4ab4ae3f365ecac2e35e49
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2026-06-15 11:07:58 -0700

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        # bfs from the treasure chest(s) to the INF land cells
+        # directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        numRows = len(grid)
+        numColumns = len(grid[0])
+        
+        def isValid(row, column):
+            if 0 <= row < numRows and 0 <= column < numColumns and grid[row][column] != -1:
+                return True
+            return False
+
+        queue = deque()
+        seen = set()
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+
+        for i in range(numRows):
+            for j in range(numColumns):
+                if grid[i][j] == 0:
+                    queue.append((i,j))
+                    seen.add((i,j))
+
+        distance = 0
+        while queue:
+            for _ in range(len(queue)):
+                row, column = queue.popleft()
+                grid[row][column] = distance
+                for dx, dy in directions:
+                    newRow, newColumn = row + dx, column + dy
+                    if isValid(newRow, newColumn) and (newRow, newColumn) not in seen:
+                        queue.append((newRow, newColumn))
+                        seen.add((newRow, newColumn))
+
+            distance += 1
