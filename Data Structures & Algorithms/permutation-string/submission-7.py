6193301fb1400544ff1c75b022683a9cb9594722
Jonathan Hung <38867001+jonachung@users.noreply.github.com>
2026-06-22 16:58:42 -0700

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,38 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # get subStringLength of s1
+        # get frequencyMap of s1
+        # sliding window to find substring that is permutation of s1
+        # keep global running frequency map
+        # if s2[right] in frequencyMap: runningMap[s2[right]] += 1
+        # if runningMap == frequencyMap: return True
+        # while (right - left + 1) > subStringLength:
+        #    if s2[left] in frequencyMap -> runningMap[s2[right]] -= 1
+        #.   left += 1
+        #.return false
+
+        # O(min(len(s1), len(s2))) time
+        # O(len(s1) + len(s2)) space (frequency dictionaries mainly)
+
+
+        subStringLength = len(s1)
+        s1FrequencyMap = defaultdict(int)
+
+        for character in s1:
+            s1FrequencyMap[character] += 1
+
+        left = 0
+        runningMap = defaultdict(int)
+        for right in range(len(s2)):
+            if s2[right] in s1FrequencyMap:
+                runningMap[s2[right]] += 1
+
+            if runningMap == s1FrequencyMap:
+                return True
+
+            if (right - left + 1) == subStringLength:
+                if s2[left] in s1FrequencyMap:
+                    runningMap[s2[left]] -= 1
+                left += 1
+
+        return False
